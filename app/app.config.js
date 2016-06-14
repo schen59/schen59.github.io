b6786a639d8432812173009a2c48452c895ddad5
Shaofeng Chen <shaofeng_2010@hotmail.com>
2016-06-13 20:37:59 -0700

    use tempalte url

--- app/app.config.js
+++ app/app.config.js
@@ -4,10 +4,9 @@ angular.
   module('schenApp').
   config(['$locationProvider' ,'$routeProvider',
     function config($locationProvider, $routeProvider) {
-
       $routeProvider.
         when('/SRLab', {
-          template: '<p>SRLab</p>'
+          templateUrl: 'templates/SRLab.html'
         }).
         otherwise('/SRLab');
     }
